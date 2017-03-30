8e0b4d8ee30f1b8aabfcec03c29f17a21b9e6e96
swetavahia2117 <sweta.vahia@rapidqube.com>
2017-03-30 10:06:57 +0530

    updating routerfile

--- app/router.js
+++ app/router.js
@@ -7,21 +7,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-<<<<<<< HEAD
-   this.route('home');
-   this.route('login');
-   this.route('sign-up');
-   this.route('coming-soon');
-   this.route('start-campaign');
-   this.route('reward');
-=======
+
   this.route('home');
   this.route('login');
   this.route('sign-up');
   this.route('coming-soon');
   this.route('start-campaign');
   this.route('fund-raiser');
->>>>>>> 0d2fc3b317064271bf07a6cae3b60a24cf525799
+  this.route('reward');
+
 });
 
 export default Router;
