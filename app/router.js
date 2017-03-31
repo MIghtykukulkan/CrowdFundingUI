ae282117942856d6326b0bbd5c7b84f3895e6146
swetavahia2117 <sweta.vahia@rapidqube.com>
2017-03-31 12:58:36 +0530

    updating router.js for logout

--- app/router.js
+++ app/router.js
@@ -7,37 +7,38 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('home');
-  this.route('login');
-  this.route('sign-up');
-  this.route('coming-soon');
-  this.route('start-campaign');
-  this.route('reward');
-  this.route('fund-raiser');
-  this.route('service-provider', function() {
-      this.route('participants', {
-          path: '/participants'
-      });
-      this.route('campaigns', {
-          path: '/campaigns'
-      });
-  });
-  this.route('browse');
-  this.route('funder-history');
-  this.route('fund-raiser', function(){
-      this.route('list-approve-bids', {
-          path: '/list-approve-bids'
-      });
-      this.route('status-of-fund-transfer', {
-          path: '/status-of-fund-transfer'
-      });
-      this.route('execute-campaign', {
-          path: '/execute-campaign'
-      });
-      this.route('deliver-commitments', {
-          path: '/deliver-commitments'
-      });
-  })
+    this.route('home');
+    this.route('login');
+    this.route('sign-up');
+    this.route('coming-soon');
+    this.route('start-campaign');
+    this.route('reward');
+    this.route('fund-raiser');
+    this.route('service-provider', function() {
+        this.route('participants', {
+            path: '/participants'
+        });
+        this.route('campaigns', {
+            path: '/campaigns'
+        });
+    });
+    this.route('browse');
+    this.route('funder-history');
+    this.route('campaign-detail');
+    this.route('fund-raiser', function() {
+        this.route('list-approve-bids', {
+            path: '/list-approve-bids'
+        });
+        this.route('status-of-fund-transfer', {
+            path: '/status-of-fund-transfer'
+        });
+        this.route('execute-campaign', {
+            path: '/execute-campaign'
+        });
+        this.route('deliver-commitments', {
+            path: '/deliver-commitments'
+        });
+    })
 });
 
 export default Router;
