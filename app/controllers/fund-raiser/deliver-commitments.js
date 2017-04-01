c62f466232d7b7b513e2f7c1c36d9e1dbc7a7966
rakeshkumarb <rakesh.bharati@rapidqube.com>
2017-04-01 14:20:00 +0530

    create action button on list & approve bids page,and change curren page no for every datatable of fund-raiser

--- app/controllers/fund-raiser/deliver-commitments.js
+++ app/controllers/fund-raiser/deliver-commitments.js
@@ -2,19 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     columns: [ {
-        "propertyName": "campaignId", "title": "Campaign Id"
+        "propertyName": "campaignId", "title": "Campaign Id", "filterPlaceholder": "Campaign Id"
     }
     , {
-        "propertyName": "campaigndetails", "title": "Campaign Details"
+        "propertyName": "campaigndetails", "title": "Campaign Details", "filterPlaceholder": "Campaign Details"
     }
     , {
-        "propertyName": "fundername", "title": "Funder Name"
+        "propertyName": "fundername", "title": "Funder Name", "filterPlaceholder": "Funder Name"
     }
     , {
-        "propertyName": "donation", "title": "Donation"
+        "propertyName": "donation", "title": "Donation", "filterPlaceholder": "Donation"
     }
     ,{
-        "propertyName": "eligible", "title": "Eligible"
+        "propertyName": "eligible", "title": "Eligible", "filterPlaceholder": "Eligible"
     }
     , ],
 });
--- app/routes/fund-raiser/status-of-fund-transfer.js
+++ app/routes/fund-raiser/status-of-fund-transfer.js
@@ -3,13 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
      model() {
         var data=[ {
-            "campaignId": "125", "campaignname": "xyz", "fundername": "Rahul", "funderamount": "300"
+            "campaignname": "xyz", "fundername": "Rahul", "funderamount": "300", "status": "success"
         }
         , {
-            "campaignId": "126", "campaignname": "abc", "fundername": "Rajesh", "funderamount": "400"
+            "campaignname": "abc", "fundername": "Rajesh", "funderamount": "400", "status": "pending"
         }
         , {
-            "campaignId": "127", "campaignname": "rtu", "fundername": "Rakesh", "funderamount": "500"
+            "campaignname": "rtu", "fundername": "Rakesh", "funderamount": "500", "status": "success"
         }
         ];
         return data;
