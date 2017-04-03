9c1b7a61234623c4538fe25b142d65fe3363c4a9
Vikramv <Vikramv@corp.rpqb.com>
2017-04-03 22:50:22 +0530

    Implemented clickable functionality to each category in the browse module

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,6 +5,21 @@ export default Ember.Controller.extend({
         logout: function() {
             sessionStorage.clear();
             window.location.reload(true);
+        },
+        
+        goTo: function(arg) {
+            if (arg === "education")
+                this.transitionToRoute('education');
+            else if (arg === 'children')
+                this.transitionToRoute('children');
+            else if (arg === 'animal-welfare')
+                this.transitionToRoute('animal-welfare');
+            else if (arg === 'environment')
+                this.transitionToRoute('environment');
+            else if (arg === 'film')
+                this.transitionToRoute('film');
+            else if (arg === 'dance')
+                this.transitionToRoute('dance');
         }
     }
 });
---
+++ app/controllers/film.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+        campaignDetails: function(arg, arg1, arg2) {
+            var imgURL = arg;
+            var category = arg1;
+            var c_title = arg2;
+            sessionStorage.setItem('imgURL', arg);
+            sessionStorage.setItem('category', arg1);
+            sessionStorage.setItem('c_title', arg2);
+            //console.log('IMG: '+ arg);
+            console.log(imgURL);
+            console.log(category);
+            console.log(c_title);
+            this.transitionToRoute('campaign-detail');
+        }
+    }
+});
