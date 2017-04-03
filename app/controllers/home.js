d27fe5c1dbc9ad62f0dff4d0114711ce17f5488b
rakeshkumarb <rakesh.bharati@rapidqube.com>
2017-04-03 17:42:16 +0530

    creating a action button in Execute Campaign

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,26 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-     actions: {
-        approvebids:function(){
-
-            this.transitionToRoute('list-approve-bids');
-
-},
-fundtransfer:function(){
-
-            this.transitionToRoute('status-of-fund-transfer');
-
-},
-executecampaign:function(){
-
-            this.transitionToRoute('execute-campaign');
-
-},
-delivercommitments:function(){
-
-            this.transitionToRoute('deliver-commitments');
-
-}
-     },
 });
