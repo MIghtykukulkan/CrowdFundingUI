1f2976759e11e9d440366409e1533ed7d8835aed
swetavahia2117 <sweta.vahia@rapidqube.com>
2017-04-05 14:28:56 +0530

    updateing reward design

--- app/controllers/campaign-detail.js
+++ app/controllers/campaign-detail.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    actions :
+    {
+        callReward : function()
+        {
+            this.transitionToRoute('reward');
+        }
+    }
 });
