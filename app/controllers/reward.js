a4197b43f48270365ef7dab187d8bc4f45b41eb1
Vikramv <Vikramv@corp.rpqb.com>
2017-04-08 14:50:48 +0530

    Payment gateway home page incorporated

--- app/controllers/reward.js
+++ app/controllers/reward.js
@@ -1,28 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
     actions: {
+        rewardSelection: function(arg, arg1) {
+            var amount = arg;
+            var reward = arg1;
 
-           rewardSelection: function(arg, arg1) {
-            var amount= arg;
-            var reward= arg1;
-            if (amount==="nill")
-            {
-            this.set('message', "You have not selected any rewards. Are you sure to continue?");
-            }
-            else{
-            this.set('message', "You have selected Rs. "+ amount +" and You will get "+ reward + " reward. Do you want to continue?");
+            if (amount === "nill") {
+                this.set('message', "You have not selected any rewards. Are you sure to continue?");
+            } else {
+                this.set('message', "You have selected Rs. " + amount + " and You will get " + reward + " reward. Do you want to continue?");
             }
             //console.log('IMG: '+ arg);
             console.log(amount);
             console.log(reward);
-           //this.transitionToRoute('campaign-detail');
+            //this.transitionToRoute('campaign-detail');
         },
-        payment : function()
-        {
-            alert("Payment Successfully");
-        }
 
+        payment: function() {
+            window.location.replace("https://www.billdesk.com/savepgi/");
+        }
     }
 });
