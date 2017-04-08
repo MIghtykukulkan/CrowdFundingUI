906855cefd5f225a1a80a1cbda3d5370cfa55483
rakeshkumarb <rakesh.bharati@rapidqube.com>
2017-04-08 16:27:31 +0530

    create pie chart

--- app/controllers/service-provider/report.js
+++ app/controllers/service-provider/report.js
@@ -2,34 +2,34 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     // Used only for time series chart
-    timeSeriesBarContent: [{
-        time: d3.time.format('%Y-%m-%d').parse("2017-01-15"),
+    content: [{
+        
         label: "New Campaigns",
         value: 10,
         type: "total"
     }, {
-        time: d3.time.format('%Y-%m-%d').parse("2017-01-15"),
+        
         label: "Closed Campaigns",
         value: 20,
         type: "total"
     }, {
-        time: d3.time.format('%Y-%m-%d').parse("2017-01-15"),
+        
         label: "In-Progress Campaigns",
         value: 9,
         type: "total"
     }, {
-        time: d3.time.format('%Y-%m-%d').parse("2017-02-15"),
-        label: "New Campaigns",
+       
+        label: "Approved Bids",
         value: 15,
         type: "total"
     }, {
-        time: d3.time.format('%Y-%m-%d').parse("2017-02-15"),
-        label: "Closed Campaigns",
+        
+        label: "Total Fund Initiated",
         value: 5,
         type: "total"
     }, {
-        time: d3.time.format('%Y-%m-%d').parse("2017-02-15"),
-        label: "In-Progress Campaigns",
+        
+        label: "Total Fund Received",
         value: 10,
         type: "total"
     }]
