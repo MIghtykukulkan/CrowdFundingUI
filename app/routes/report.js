f23d34a4d0c3f2e3395fa5aa7a17eca0ba50a0f4
swetavahia2117 <sweta.vahia@rapidqube.com>
2017-04-13 12:21:51 +0530

    updating images

--- app/routes/report.js
+++ app/routes/report.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
             "campaignname": "Seed Funding",  "pledgeamount": "10000/-", "receivedamount": "10000/-","fundsreceived": "100%", "status": "Received"
         }
         , {
-           "campaignname": "Hamari Nadi",  "pledgeamount": "15000/-", "receivedamount": "0","fundsreceived": "66.67%", "status": "Pending"
+           "campaignname": "Hamari Ganga ",  "pledgeamount": "15000/-", "receivedamount": "0","fundsreceived": "66.67%", "status": "Pending"
         }
         , {
             "campaignname": "Plant Tree",  "pledgeamount": "20000/-", "receivedamount": "20000/-","fundsreceived": "70%","status": "Not Received"
