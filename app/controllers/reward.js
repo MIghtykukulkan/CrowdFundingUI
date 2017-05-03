0bd2ce218f9d421df8f0c9f9136372c9163cbbea
swetavahia2117 <sweta.vahia@rapidqube.com>
2017-05-03 18:24:09 +0530

    Reward Condition

--- app/controllers/reward.js
+++ app/controllers/reward.js
@@ -5,16 +5,18 @@ export default Ember.Controller.extend({
     isShowingModal: false,
 
     actions: {
+        noRewardSelection: function(arg, arg1) {
+            var amount = arg;
+            var reward = arg1;
+            this.set('message', "You have not selected any rewards and you want to contribute Rs "+amount+"?");
+           },
+
         rewardSelection: function(arg, arg1) {
             var amount = arg;
             var reward = arg1;
-
-            if (amount === "nill") {
-                this.set('message', "You have not selected any rewards. Are you sure to continue?");
-            } else {
-                this.set('message', "You have selected Rs. " + amount + " and You will get " + reward + " reward. Do you want to continue?");
-            }
+            this.set('message', "You have selected Rs. " + amount + " and You will get " + reward + " reward. Do you want to continue?");
         },
+        
          toggleModal: function() {
             this.toggleProperty('isShowingModal');
         },
