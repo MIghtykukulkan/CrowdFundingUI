cb7e362363ddc6f0753bb4350a358cb3d4f6c29e
rakeshkumarb <rakesh.bharati@rapidqube.com>
2017-05-06 18:06:07 +0530

    resolve issue of start campaign page

--- app/controllers/start-campaign.js
+++ app/controllers/start-campaign.js
@@ -38,7 +38,7 @@ var Validations = buildValidations({
      rewardtitle: [
          validator('presence', true),
         validator('format', {
-            regex: /^[A-Za-z ]+$/
+            regex: /^[A-Za-z/-/-_/ ]+$/
         })
     ],
 
@@ -89,6 +89,30 @@ export default Ember.Controller.extend(Validations,{
         },
 
         saveRewards: function() {
+            var rewardtitle = this.get('rewardtitle');
+            var rewardamount = this.get('rewardamount');
+            var rewarddescription = this.get('rewarddescription');
+            var deliveryDate = this.get('deliveryDate');
+            
+            if (rewardtitle === null || rewardtitle === undefined || rewardtitle === "") {
+                this.set('rewardtitleerrormessage', "field cannot be empty")
+                return;
+            }
+
+             if (rewardamount === null || rewardamount === undefined || rewardamount === "") {
+                this.set('rewardamounterrormessage', "field cannot be empty")
+                return;
+            }
+
+             if (rewarddescription === null || rewarddescription === undefined || rewarddescription === "") {
+                this.set('rewarddescriptionerrormessage', "field cannot be empty")
+                return;
+            }
+
+            if (deliveryDate === null || deliveryDate === undefined || deliveryDate === "") {
+                this.set('dateerrormessage', "Date field cannot be empty")
+                return;
+            }
             this.set('isAddReward', false);
             this.toggleProperty('isSaveReward');
         }
