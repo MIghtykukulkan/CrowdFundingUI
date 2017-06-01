ca6aec450e6c2e61f194f23d1ba4d5051eaca737
Rahuld <Rahuld@corp.rpqb.com>
2017-06-01 18:56:01 +0530

    update

--- app/routes/fund-raiser/status-of-fund-transfer.js
+++ app/routes/fund-raiser/status-of-fund-transfer.js
@@ -5,21 +5,21 @@ export default Ember.Route.extend({
         var data = [{
             "campaignname": "Avarice - A short film",
             "fundername": "Lokesh Reddi",
-            "pleageamount": "300",
+            "pleageamount": "20000",
             "receivedamount": "80%",
             "rewards": "T-shirt",
             "status": "Fund Received"
         }, {
             "campaignname": "Spin Be Fit",
             "fundername": "Kavi",
-            "pleageamount": "400",
+            "pleageamount": "10000",
             "receivedamount": "70%",
             "rewards": "T-shirt",
             "status": "Initiate Campaign"
         }, {
             "campaignname": "The Rhinos Need Help",
             "fundername": "Mohanraj PB",
-            "pleageamount": "500",  
+            "pleageamount": "50000",  
             "receivedamount": "90%",
             "rewards": "T-shirt",
             "status": "Reward Delivered"
