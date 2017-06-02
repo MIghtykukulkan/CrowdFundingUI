d5f443dc4472c601283f0bfb972e674738a4325f
Rahuld <Rahuld@corp.rpqb.com>
2017-06-02 12:19:36 +0530

    add my changes

--- app/routes/home.js
+++ app/routes/home.js
@@ -10,19 +10,23 @@ export default Ember.Route.extend({
 
         if (userType === "Admin") {
             showAdmin = true;
+            this.controllerFor('home').set('isDefault', false);
             this.controllerFor('home').set('showAdmin', showAdmin);
             this.controllerFor('application').set('showAdminHeaderModules', false);
         } else if (userType === "Fundraiser") {
             showFund = true;
+            this.controllerFor('home').set('isDefault', true);
             this.controllerFor('home').set('showFund', showFund);
             this.controllerFor('application').set('showStartCampaign', true);
         } else if (userType === "Funder") {
             showFunder = true;
+            this.controllerFor('home').set('isDefault', false);
             this.controllerFor('home').set('showFunder', showFunder);
             this.controllerFor('application').set('showFunderModules', true);
             this.controllerFor('application').set('showStartCampaign', false);
         } else if (userType === "Customer") {
             showCustomer = true;
+            this.controllerFor('home').set('isDefault', false);
             this.controllerFor('home').set('showCustomer', showCustomer);
             this.controllerFor('application').set('showAdminHeaderModules', false);
         }              
